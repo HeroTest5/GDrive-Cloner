e8e4c369b6c579a4393ee85302485f8c26dbdd77
HeroTest5 <109363788+HeroTest5@users.noreply.github.com>
2022-09-11 15:37:36 +0530

    Create Command.sh

---
+++ Command.sh
@@ -0,0 +1,2 @@
+rclone sync Shared:GdriveXbot OnedriveBusiness1:GdriveXbot --config=rclone.conf --drive-acknowledge-abuse
+rclone sync Shared:HeroBenHero/Movies OnedriveBusiness1:Movies --config=rclone.conf --drive-acknowledge-abuse
